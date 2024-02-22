018e5ba9326a861dd79ff72ad2836f0f178a6e90
avery moates <avery.moates17@ou.edu>
2024-02-22 15:52:53 -0600

    Added the demo.py file.

--- train.py
+++ train.py
@@ -4,36 +4,6 @@ import sys
 from alphabeta import alphabeta
 
 def main():
-    game = TTT.TicTacToe()
-
-    copyGame = TTT.TicTacToe(board=game.get_board())
-    otherPlayer = alphabeta()
-
-    game.display_board()
-    game.start_game()
-
-    print(game.get_game_status())
-
-    current_player = game.get_current_player()
-
-    print(current_player)
-
-    moves = [2,5,8]
-    while(game.get_game_status()):
-        if current_player == 1:
-            placement = otherPlayer.alphabeta_make_move(game.get_board(),10,1)
-            game.make_move(placement,1)
-            current_player = game.get_current_player()
-            game.display_board()
-
-        elif current_player == 2:
-            print('Player 2, enter your move:')
-            placement = int(input())
-            game.make_move(placement,2)
-            current_player = game.get_current_player()
-            game.display_board()
-
-        game.check_board()
 
 if __name__ == '__main__':
     main()
